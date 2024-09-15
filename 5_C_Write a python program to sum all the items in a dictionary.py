53debc3a1315d14cab232f299530ce9f6c93771a
LUCVK1810 <lucuniversal101@gmail.com>
2024-09-15 15:47:32 +0530

    Add files via upload

---
+++ 5_C_Write a python program to sum all the items in a dictionary.py
@@ -0,0 +1,5 @@
+def sum_dict_values(my_dict):
+    return sum(my_dict.values())
+my_dict = {'a': 100, 'b': 200, 'c': 300}
+total_sum = sum_dict_values(my_dict)
+print("Sum of all items in the dictionary:", total_sum)
